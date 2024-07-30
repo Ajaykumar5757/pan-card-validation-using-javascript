fe268f91f631660e84783dcf12762a36b7141bf7
Ajaykumar A <ajaykumarak2703@gmail.com>
2024-07-30 22:49:25 +0530

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,9 @@
 function verify(){
-    let msg=document.getElementById("pan").value;
-    if(msg.length==10){
-        if(((msg[0],msg[1],msg[2],msg[3],msg[4],msg[9])>='A' && (msg[0],msg[1],msg[2],msg[3],msg[4],msg[9])<='Z') && ((msg[5],msg[6],msg[7],msg[8])>='0' && (msg[5],msg[6],msg[7],msg[8]))){
+    let msg = document.getElementById("pan").value;
+    let reg = /^([A-Z]){5}([0-9]){4}([A-Z]){1}$/;
+    if(msg.lengthy==10){
+
+        if(reg.test(msg)){
             alert("Verification successfull");
         }
         else{
@@ -12,3 +14,10 @@ function verify(){
         alert("Invalid pan number\npan number must contain 6 characters and 4 digits");
     }
 }
+
+// if(((msg[0],msg[1],msg[2],msg[3],msg[4],msg[9])>='A' && (msg[0],msg[1],msg[2],msg[3],msg[4],msg[9])<='Z') && ((msg[5],msg[6],msg[7],msg[8])>=0 && (msg[5],msg[6],msg[7],msg[8])<=9)){
+//     alert("Verification successfull");
+// }
+// else{
+//     alert("Invalid pan number");
+// }
